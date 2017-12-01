e87b35cb767f8329071c012c96fcadf1102e9e70
unknown <nfano079@uottawa.ca>
2017-11-30 20:21:45 -0500

    updates in Edit/View Chore and CreateChore

--- SEG21052/app/src/main/java/com/example/ahame_000/seg2105/CreateChoreActivity.java
+++ SEG21052/app/src/main/java/com/example/ahame_000/seg2105/CreateChoreActivity.java
@@ -2,6 +2,7 @@ package com.example.ahame_000.seg2105;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +28,7 @@ public class CreateChoreActivity extends AppCompatActivity {
         incorrectPopUp.setVisibility(View.INVISIBLE);
 
 
-        Spinner spinner = findViewById(R.id.AssignToProfiles_Spinner_ChoreDetails);
+        Spinner spinner = findViewById(R.id.AssignTo_Spinner_CreateChore);
         List<Profile> profiles =Session.getLoggedInAccount().getChildren();
         profiles.add(Session.getLoggedInProfile());
         Profile unassignedProfile = new Adult("Unassigned","",null);
@@ -76,10 +77,12 @@ public class CreateChoreActivity extends AppCompatActivity {
         catch (Exception e ){
             penalty = 0;
         }
-        //TODO the spinner for assigning to someone
 
-
-
+        Spinner assignToSpinner = findViewById(R.id.AssignTo_Spinner_CreateChore);
+        Profile assignToProfile = (Profile)assignToSpinner.getSelectedItem();
+        if(assignToProfile.getAccount()==null){
+            assignToProfile = null;
+        }
 
         // TODO if statement in saving
 
@@ -96,45 +99,14 @@ public class CreateChoreActivity extends AppCompatActivity {
         else{
             //TODO add profile instead of null
             Chore chore = new Chore(choreName,description, null,duedate,(Adult)Session.getLoggedInProfile(),
-                    null, reward, penalty, Session.getLoggedInAccount(), UUID.randomUUID());
+                    assignToProfile, reward, penalty, Session.getLoggedInAccount(), UUID.randomUUID());
 
             DatabaseManager DM = new DatabaseManager(new DatabaseHelper(this.getApplicationContext()));
-
             DM.saveChore(chore);
-        }
-
-
-        /*
-        if(chore.getName()== null){
-            addButton.setVisibility(View.INVISIBLE);
-        }
-
-        if (chore.getPenalty() == 0){
-            addButton.setVisibility(View.INVISIBLE);
+            this.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK));
 
         }
 
-        if(chore.getReward() == 0){
-            addButton.setVisibility(View.INVISIBLE);
-
-        }
-
-        if(chore.getDeadline() == null){
-            addButton.setVisibility(View.INVISIBLE);
-
-        }
-
-        if (chore.getCreator() == null){
-            addButton.setVisibility(View.INVISIBLE);
-
-        }
-
-        if (chore.getAccount() == null){
-            addButton.setVisibility(View.INVISIBLE);
-
-        }
-
-        ****/
     }
 
 }
