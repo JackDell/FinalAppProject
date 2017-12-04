1e72e6664889e5f6d3567c34b6a5a18f573dc24e
JackDell <jdell012@uottawa.ca>
2017-12-03 23:15:09 -0500

    m

--- SEG21052/app/src/main/java/com/example/ahame_000/seg2105/DateHelper.java
+++ SEG21052/app/src/main/java/com/example/ahame_000/seg2105/DateHelper.java
@@ -1,6 +1,8 @@
 package com.example.ahame_000.seg2105;
 
 
+import android.util.Log;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -12,6 +14,8 @@ public class DateHelper {
 
     public static Date dateFromString(String date) {
         String[] info = date.split("/");
+
+        Log.d("Date", info.toString());
 
         Calendar calendar = new GregorianCalendar(Integer.parseInt(info[2]), Integer.parseInt(info[1]), Integer.parseInt(info[0]));
         return calendar.getTime();
